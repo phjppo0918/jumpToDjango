1741ec9e6eeeafe8a7e95d818f8cbbd26fc86e1d
phjppo0918 <phjppo0918@kpu.ac.kr>
2021-03-20 14:37:46 +0000

    2-03 fin

--- projects/mysite/config/settings.py
+++ projects/mysite/config/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-     
+
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'sample', # schema명

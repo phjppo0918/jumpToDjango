d5c5c86ab7e34c0d089fff749dccb33d93db40fc
phjppo0918 <phjppo0918@kpu.ac.kr>
2021-03-21 11:19:45 +0000

    2-07 fin

--- projects/mysite/pybo/views.py
+++ projects/mysite/pybo/views.py
@@ -20,16 +20,16 @@ def detail(request, question_id):
     
     """
     pybo 내용 출력
-    """
-    question =get_object_or_404(Question, pk=question_id)
+    """ 
+    question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     
-    return render(request, 'pybo/question_detail.html',context)
+    return render(request, 'pybo/question_detail.html', context)
 
 def answer_create(request, question_id):
     """
     pybo 답변등록
     """
-    question =get_object_or_404(Question, pk=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
     return redirect('pybo:detail', question_id=question.id)

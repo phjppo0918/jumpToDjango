9de407a9d1fceeb1a44899a86a6b90b99935f412
phjppo0918 <phjppo0918@kpu.ac.kr>
2021-03-24 14:19:11 +0000

    3-11 fin

--- projects/mysite/config/urls.py
+++ projects/mysite/config/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from pybo import views
+from pybo.views import base_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path('', views.index, name='index'),
+    path('', base_views.index, name='index'),  # '/' 에 해당되는 path
     path('pybo/', include('pybo.urls')),
     path('common/', include('common.urls')),
     
